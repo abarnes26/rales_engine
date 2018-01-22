d1772b9f809a208fa6faad387fa40ab80fb3e288
abarnes26 <abarnes26@yahoo.com>
2018-01-22 16:47:42 -0700

    built migrations except invoice_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180122221943) do
+ActiveRecord::Schema.define(version: 20180122233924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,44 @@ ActiveRecord::Schema.define(version: 20180122221943) do
     t.string "updated_at"
   end
 
+  create_table "invoices", force: :cascade do |t|
+    t.bigint "customer_id"
+    t.bigint "merchant_id"
+    t.string "status"
+    t.string "created_at"
+    t.string "updated_at"
+    t.index ["customer_id"], name: "index_invoices_on_customer_id"
+    t.index ["merchant_id"], name: "index_invoices_on_merchant_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.float "unit_price"
+    t.bigint "merchant_id"
+    t.string "created_at"
+    t.string "updated_at"
+    t.index ["merchant_id"], name: "index_items_on_merchant_id"
+  end
+
+  create_table "merchants", force: :cascade do |t|
+    t.string "name"
+    t.string "created_at"
+    t.string "updated_at"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "invoice_id"
+    t.string "credit_card_number"
+    t.string "credit_card_expiration_date"
+    t.string "result"
+    t.string "created_at"
+    t.string "updated_at"
+    t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
+  end
+
+  add_foreign_key "invoices", "customers"
+  add_foreign_key "invoices", "merchants"
+  add_foreign_key "items", "merchants"
+  add_foreign_key "transactions", "invoices"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,28 @@
 
   require 'csv'
 
+  Invoice.delete_all
+  Item.delete_all
   Customer.delete_all
+  Merchant.delete_all
+  Transaction.delete_all
 
   customer_csv_text = File.read('./data/customers.csv')
   csv = CSV.parse(customer_csv_text, :headers => true)
   csv.each do |row|
     Customer.create!(row.to_hash)
+  end
+
+  merchant_csv_text = File.read('./data/merchants.csv')
+  csv = CSV.parse(merchant_csv_text, :headers => true)
+  csv.each do |row|
+    Merchant.create!(row.to_hash)
+  end
+
+  item_csv_text = File.read('./data/items.csv')
+  csv = CSV.parse(item_csv_text, :headers => true)
+  csv.each do |row|
+    Item.create!(row.to_hash)
   end
 
   invoice_csv_text = File.read('./data/invoices.csv')
@@ -22,8 +38,10 @@
     Invoice.create!(row.to_hash)
   end
 
-  # item_invoice_csv_text = File.read('./data/item_invoices.csv')
-  # csv = CSV.parse(item_invoice_csv_text, :headers => true)
-  # csv.each do |row|
-  #   ItemInvoice.create!(row.to_hash)
-  # end
+  transaction_csv_text = File.read('./data/transactions.csv')
+  csv = CSV.parse(transaction_csv_text, :headers => true)
+  csv.each do |row|
+    Transaction.create!(row.to_hash)
+  end
+
+  puts "Seed Complete"
