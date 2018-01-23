b45e20105b04059e5bbe9737172518c42abe3022
abarnes26 <abarnes26@yahoo.com>
2018-01-22 21:24:31 -0700

    merchant, item, and customer api controller created. Tests passing for index and show

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
       namespace :v1 do
         resources :items, only: [:index, :show]
         resources :merchants, only: [:index, :show]
-        resources :customer, only: [:index, :show]
-        resources :invoice, only: [:index, :show]
-        resources :transaction, only: [:index, :show]
+        resources :customers, only: [:index, :show]
+        resources :invoices, only: [:index, :show]
+        resources :transactions, only: [:index, :show]
       end
     end
   end
---
+++ spec/factories/customers.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :customer do
+    first_name "CustomerFirstName"
+    last_name "CustomerLasttName"
+  end
+end
