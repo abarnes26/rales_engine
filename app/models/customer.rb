4d62362c74fc23c88efa656bfc3f91e854441490
abarnes26 <abarnes26@yahoo.com>
2018-01-22 18:13:38 -0700

    customer model tests passing

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  validates_presence_of :first_name, :last_name, :updated_at, :created_at
+  validates_presence_of :first_name, :last_name
   has_many :invoices
 
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,1 +1,13 @@
+require 'rails_helper'
 
+describe Customer, type: :model do
+  describe "Validations" do
+    it {is_expected.to validate_presence_of(:first_name)}
+    it {is_expected.to validate_presence_of(:last_name)}
+  end
+
+  describe "Relationships" do
+    it {is_expected.to have_many(:invoices)}
+  end
+
+end
