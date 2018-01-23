48e624d3ca413aa857c353b4c6f95032ebf20f3f
abarnes26 <abarnes26@yahoo.com>
2018-01-23 13:55:10 -0700

    updated strong params for invoice item find controller

--- app/controllers/api/v1/invoice_items/find_controller.rb
+++ app/controllers/api/v1/invoice_items/find_controller.rb
@@ -1,17 +1,17 @@
 class Api::V1::InvoiceItems::FindController < ApplicationController
 
   def show
-    render json: Customer.find_by(find_params)
+    render json: InvoiceItems.find_by(find_params)
   end
 
   def index
-    render json: Customer.where(find_params)
+    render json: InvoiceItems.where(find_params)
   end
 
   private
 
   def find_params
-    params.permit(:first_name, :last_name, :created_at, :updated_at)
+    params.permit(:quantity, :unit_price, :created_at, :updated_at)
   end
 
 end
