6eaecbea32c6e43b062480aad8e988fbb83b6909
abarnes26 <abarnes26@yahoo.com>
2018-01-23 13:52:36 -0700

    added find controllers for all models. added namespacing to routes

---
+++ app/controllers/api/v1/items/find_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::Items::FindController < ApplicationController
+
+  def show
+    render json: Customer.find_by(find_params)
+  end
+
+  def index
+    render json: Customer.where(find_params)
+  end
+
+  private
+
+  def find_params
+    params.permit(:first_name, :last_name, :created_at, :updated_at)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,35 @@ Rails.application.routes.draw do
           get "find_all", to: "find#index"
         end
         resources :customers, only: [:index, :show]
+
+        namespace :items do
+          get "find", to: "find#show"
+          get "find_all", to: "find#index"
+        end
         resources :items, only: [:index, :show]
+
+        namespace :merchants do
+          get "find", to: "find#show"
+          get "find_all", to: "find#index"
+        end
         resources :merchants, only: [:index, :show]
+
+        namespace :invoices do
+          get "find", to: "find#show"
+          get "find_all", to: "find#index"
+        end
         resources :invoices, only: [:index, :show]
+
+        namespace :transactions do
+          get "find", to: "find#show"
+          get "find_all", to: "find#index"
+        end
         resources :transactions, only: [:index, :show]
+
+        namespace :invoice_items do
+          get "find", to: "find#show"
+          get "find_all", to: "find#index"
+        end
         resources :invoice_items, only: [:index, :show]
       end
     end
